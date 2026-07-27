51b89f378e144b93cdb7e82ba22ec22a3d3b84d4
abdussomad <abdussomad@abdussomads-MacBook-Air.local>
2026-07-27 21:21:35 +0800

    Fit four main tabs on iPhone

--- api_handlers/app.js
+++ api_handlers/app.js
@@ -2474,7 +2474,7 @@ function pageHtml() {
       }
 
       .dashboard-workspace .quick-grid {
-        grid-template-columns: repeat(3, minmax(0, 1fr));
+        grid-template-columns: repeat(4, minmax(0, 1fr));
       }
     }
 
